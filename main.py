9f76df69315fd1ccbfd7da04ed761ced385ce0cc
Akooshsoohoo <1sahakash1@gmail.com>
2026-04-19 17:24:37 -0400

    extract filename from json metadata

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import json
 
 def main():
     path = "data/sample.json"   
-     
     with open(path, "r") as f:
             data = json.load(f)
     
-    print (data)
+    filename = data ["originalFilename"]
+    print (filename)
     
 if __name__ == "__main__":
     main()
