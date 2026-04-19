7bb77980f689e47325d306a2af78130c3c1e9983
Akooshsoohoo <1sahakash1@gmail.com>
2026-04-19 17:34:11 -0400

    can not process multiple json files and collect records, added additional json test file

--- main.py
+++ main.py
@@ -1,21 +1,32 @@
 import json
+import os
 
 def main():
-    path = "data/sample.json"   
-    with open(path, "r") as f:
-            data = json.load(f)
-    
-    filename = data ["originalFilename"]
-    timestamp = data["photoTakenTime"]["timestamp"]
-    file_type = data["mimeType"]
-    
-    photo_record = {
-        "filename": filename,
-        "timestamp": timestamp,
-        "type": file_type
-    }
-    
-    print (photo_record)
-    
+    folder = "data"  # folder containing json files
+    records = []  # store all photo records
+
+    for file_name in os.listdir(folder):
+        if file_name.endswith(".json"):
+            path = os.path.join(folder, file_name)
+
+            with open(path, "r") as f:
+                data = json.load(f)
+
+            # extract fields
+            filename = data["originalFilename"]
+            timestamp = data["photoTakenTime"]["timestamp"]
+            file_type = data["mimeType"]
+
+            # create structured record
+            photo_record = {
+                "filename": filename,
+                "timestamp": timestamp,
+                "type": file_type
+            }
+
+            records.append(photo_record)  # add to list
+
+    print(records)  
+
 if __name__ == "__main__":
     main()
